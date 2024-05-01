aa6073538b9ef9712e375b182e04bd28090120d4
nehavalliani09 <168576701+nehavalliani09@users.noreply.github.com>
2024-05-01 17:47:09 +0000

    assign1 updated

--- authorship-verification-trivial/authorship_verification_trivial.py
+++ authorship-verification-trivial/authorship_verification_trivial.py
@@ -1,40 +1,24 @@
 from pathlib import Path
-
+import json
+from sklearn.metrics import precision_score, recall_score, f1_score
 from tira.rest_api_client import Client
 from tira.third_party_integrations import get_output_directory
 
-if __name__ == "__main__":
-
+def load_data(dataset_name, dataset_type):
     tira = Client()
+    text = tira.pd.inputs("nlpbuw-fsu-sose-24", dataset_name)
+    targets = tira.pd.truths("nlpbuw-fsu-sose-24", dataset_name)
+    return text, targets
 
-    # loading train data
-    text_train = tira.pd.inputs(
-        "nlpbuw-fsu-sose-24", "authorship-verification-train-20240408-training"
-    )
-    targets_train = tira.pd.truths(
-        "nlpbuw-fsu-sose-24", "authorship-verification-train-20240408-training"
-    )
-    # loading validation data (automatically replaced by test data when run on tira)
-    text_validation = tira.pd.inputs(
-        "nlpbuw-fsu-sose-24", "authorship-verification-validation-20240408-training"
-    )
-    targets_validation = tira.pd.truths(
-        "nlpbuw-fsu-sose-24", "authorship-verification-validation-20240408-training"
-    )
+def calculate_metrics(predictions, ground_truth):
+    precision = precision_score(ground_truth, predictions)
+    recall = recall_score(ground_truth, predictions)
+    f1 = f1_score(ground_truth, predictions)
+    return precision, recall, f1
 
-    # classifying the data
-    prediction = (
-        text_validation.set_index("id")["text"]
-        .str.contains("delve", case=False)
-        .astype(int)
-    )
+if __name__ == "__main__":
+    # Load train and validation data
+    train_text, train_targets = load_data("authorship-verification-train-20240408-training", "train")
+    validation_text, validation_targets = load_data("authorship-verification-validation-20240408-training", "validation")
 
-    # converting the prediction to the required format
-    prediction.name = "generated"
-    prediction = prediction.reset_index()
 
-    # saving the prediction
-    output_directory = get_output_directory(str(Path(__file__).parent))
-    prediction.to_json(
-        Path(output_directory) / "predictions.jsonl", orient="records", lines=True
-    )
