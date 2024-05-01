80f2ec073e4630fcf928e343cfe9c18c768fac28
nehavalliani09 <168576701+nehavalliani09@users.noreply.github.com>
2024-05-01 17:55:36 +0000

    assignment 1 done

--- authorship-verification-bayes/run.py
+++ authorship-verification-bayes/run.py
@@ -11,24 +11,18 @@ def load_data(dataset_name):
     return data
 
 if __name__ == "__main__":
-    # Load the test data
+    # Load the test and trained data
     test_data = load_data("authorship-verification-validation-20240408-training")
     print("Test data:", test_data)
-
-    # Load the trained model
     model = load("model.joblib")
-
-    # Make predictions
-    predictions = model.predict(test_data["text"])
-
-    # Save the predictions
+    predictions = model.predict(test_data["text"]) #making predictions
     output_directory = get_output_directory(str(Path(__file__).parent))
-    with open(Path(output_directory) / "predictions.jsonl", "w", encoding="utf-8") as file:
+    with open(Path(output_directory) / "predictions.jsonl", "w", encoding="utf-8") as file: #prediction stored in jsonl file
         for sample_id, label in zip(test_data["id"], predictions):
             file.write(json.dumps({"id": sample_id, "generated": int(label)}) + "\n")
 
 
-    # Calculate F1 score
+    # Calculating F1 score
     ground_truth = [int(sample_id) % 2 for sample_id in test_data["id"]]
     f1 = f1_score(ground_truth, predictions)
 
