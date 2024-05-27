b0a317d2d4a0eadeb525d7f994778b7bf6cd355b
azamin07 <168576232+azamin07@users.noreply.github.com>
2024-05-27 12:47:26 +0000

    done

--- paraphrase-identification submimssion/run.py
+++ paraphrase-identification submimssion/run.py
@@ -1,7 +1,7 @@
 from pathlib import Path
 from tira.rest_api_client import Client
 from tira.third_party_integrations import get_output_directory
-import joblib  # For loading the model and vectorizer
+import joblib  
 
 def load_data(tira, dataset_name):
     text = tira.pd.inputs("nlpbuw-fsu-sose-24", dataset_name).set_index("id")
